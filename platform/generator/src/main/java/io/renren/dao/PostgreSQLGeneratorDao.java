a9a811a7fab523181fe418c2c6a0116ebce19d1f
weixiang <463460902@qq.com>
2021-05-11 14:41:07 +0800

    增加代码生成器，集成初始Vue页面

---
+++ platform/generator/src/main/java/io/renren/dao/PostgreSQLGeneratorDao.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 
+ */
+
+package io.renren.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * PostgreSQL代码生成器
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@Mapper
+public interface PostgreSQLGeneratorDao extends GeneratorDao {
+
+}
