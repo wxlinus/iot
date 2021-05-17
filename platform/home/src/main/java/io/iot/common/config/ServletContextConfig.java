0f7ed13c62aacd89130891093da3ec3f4cb65599
weixiang <463460902@qq.com>
2021-05-17 23:20:26 +0800

    解决第一次访问地址栏出现JSESSIONID

---
+++ platform/home/src/main/java/io/iot/common/config/ServletContextConfig.java
@@ -0,0 +1,28 @@
+package io.iot.common.config;
+
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
+import java.util.Collections;
+
+@Configuration
+//解决第一次访问地址栏出现JSESSIONID
+public class ServletContextConfig {
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+        return new ServletContextInitializer() {
+            @Override
+            public void onStartup(ServletContext servletContext) throws ServletException {
+                servletContext.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE));
+                SessionCookieConfig sessionCookieConfig=servletContext.getSessionCookieConfig();
+                sessionCookieConfig.setHttpOnly(true);
+            }
+        };
+
+    }
+}
